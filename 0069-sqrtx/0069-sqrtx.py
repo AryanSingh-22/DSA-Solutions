8542df4aed759b999be0be25b5dd9ab97be58108
Aryan Singh <aryansingh95488@gmail.com>
2026-07-17 13:45:39 +0530

    Time: 3 ms (66.15%), Space: 19.3 MB (23.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,10 +3,8 @@ class Solution:
         start = 1
         end = x
         ans = 0
-
         while start <= end:
             mid = start + (end - start) // 2
-
             if mid * mid == x:
                 ans = mid
                 break
@@ -15,5 +13,4 @@ class Solution:
                 ans = mid
             else:
                 end = mid - 1
-
         return ans
